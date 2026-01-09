61d576128c395a7c10583853404c5d95ef90dddd
Jonathan Sutcliffe <jonathan.sutcliffe@gov.scot>
2026-01-09 10:32:43 +0000

    Storybook UI font size overrides

--- .storybook/sgdsTheme.ts
+++ .storybook/sgdsTheme.ts
@@ -6,4 +6,11 @@ export default create({
   brandUrl: 'https://designsystem.gov.scot',
   brandImage: '/images/scottish-government.svg',
   brandTarget: '_self',
+
+  colorSecondary: '#0065bd',
+  appBg: '#f8f8f8',
+  appBorderColor: '#b3b3b3',
+  appBorderRadius: 0,
+  textColor: '#1a1a1a',
+  barTextColor: '#333e48'
 });
