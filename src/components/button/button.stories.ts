42c7f279422387700081f16666ea9cbdd6408c02
David Kindness <david.kindness@gov.scot>
2026-03-05 11:34:10 +0000

    Add button group

--- src/components/button/button.stories.ts
+++ src/components/button/button.stories.ts
@@ -8,19 +8,19 @@ type ButtonWidth = 'fluid' | 'fixed' | 'max';
 
 export type ButtonArgs = {
     content: string
-    hasLinkStyle?: boolean;
-    href?: string;
-    icon?: string;
+    hasLinkStyle?: boolean
+    href?: string
+    icon?: string
     isDisabled?: boolean
-    isIconLeft?: boolean;
-    isIconOnly?: boolean;
-    isSecondary?: boolean;
-    isSmall?: boolean;
-    width?: ButtonWidth;
+    isIconLeft?: boolean
+    isIconOnly?: boolean
+    isSecondary?: boolean
+    isSmall?: boolean
+    width?: ButtonWidth
 };
 
 const meta: Meta<ButtonArgs> = {
-    title: 'Components/Button',
+    title: 'Components/Button/Button',
     render: (args) => {
         return(ComponentTemplate(args))
     },
