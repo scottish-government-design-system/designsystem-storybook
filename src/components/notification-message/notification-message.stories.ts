a71acf5228513e1ddd6a5c4dff807ad6a46a22ff
David Kindness <david.kindness@gov.scot>
2026-05-07 16:32:15 +0100

    DSP-1595 Add more stories for notification message
    Update icon stack sprite SVG

--- src/components/notification-message/notification-message.stories.ts
+++ src/components/notification-message/notification-message.stories.ts
@@ -7,7 +7,9 @@ import ComponentTemplate from './notification-message.njk';
 export type NotificationMessageArgs = {
     heading: string,
     headingLevel?: 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
-    content: string
+    isDismissable?: boolean,
+    content: string,
+    state?: 'success' | 'error' | 'warning' | 'info'
 };
 
 const meta: Meta<NotificationMessageArgs> = {
@@ -22,21 +24,33 @@ const meta: Meta<NotificationMessageArgs> = {
         content: "<p>You have added a tenant to the application.</p>"
     },
     argTypes: {
-        headingLevel: SGDSArgTypes.headingLevel()
+        headingLevel: SGDSArgTypes.headingLevel(),
+        isDismissable: SGDSArgTypes.boolean({
+            description: 'Whether to show a close button on the notification'
+        }),
+        state: SGDSArgTypes.state()
+    },
+    play: () => {
+        window.DS.initAll();
     }
 };
 
 export default meta;
 type Story = StoryObj<NotificationMessageArgs>;
 
-export const Default: Story = {};
+export const Default: Story = {
+    parameters: {
+        docs: {
+            story: { autoplay: true },
+        }
+    }
+};
 
 export const NoContent: Story = {
     name: 'No content',
     args: {
         content: undefined
     }
-
 };
 
 export const LinksInContent: Story = {
@@ -44,5 +58,41 @@ export const LinksInContent: Story = {
     args: {
         content: `<p>You have added a tenant to the <a href="#">application</a>.
         Go to the <a href="#">tenant list</a> to view all tenants.</p>`
+    }
+};
+
+export const CloseButton: Story = {
+    name: 'Close button',
+    args: {
+        content: `<p>You have added a tenant to the <a href="#">application</a>.
+        Go to the <a href="#">tenant list</a> to view all tenants.</p>`,
+        isDismissable: true
+    }
+};
+
+export const InfoState: Story = {
+    name: 'Info state',
+    args: {
+        heading: 'Welcome to the mygov.scot app',
+        state: 'info',
+        content: `<p>This is a beta version. Not all features will be available.</p>`
+    }
+};
+
+export const ErrorState: Story = {
+    name: 'Error state',
+    args: {
+        heading: 'Something went wrong',
+        state: 'error',
+        content: `<p>That link does not seem to be working.<br>Please try again.</p>`
+    }
+};
+
+export const WarningState: Story = {
+    name: 'Warning state',
+    args: {
+        heading: 'Partial name match',
+        state: 'warning',
+        content: `<p>Please confirm that the name on the account is correct.</p>`
     }
 };
